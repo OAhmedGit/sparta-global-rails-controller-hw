7a9224e180348d622a9bc5986fd07c984dcbbc46
Osama Ahmed <oahmed@spartaglobal.com>
2018-05-14 19:11:57 +0100

    Created team pages showing the players, and links to other team pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    get "mancity/kdb", to: "mancity#kdb", as: 'kdb'
-   get "mancity/aguero", to: "mancity#aguero", as:'aguero'
+   get "mancity/aguero", to: "mancity#aguero", as: 'aguero'
    get "mancity/kompany", to: "mancity#kompany", as: 'kompany'
    get "mancity/ederson", to: "mancity#ederson", as: 'ederson'
+   get "mancity", to: "mancity#all_mancity", as: 'all_mancity'
 
-   get "arsenal/monreal", to: "mancity#monreal", as:'monreal'
-   get "arsenal/wilshere", to: "mancity#wilshere", as:'wilshere'
-   get "arsenal/aubameyang", to: "mancity#aubameyang", as:'aubameyang'
-   get "arsenal/lacazette", to: "mancity#lacazette", as:'lacazette'
+   get "arsenal/monreal", to: "arsenal#monreal", as:'monreal'
+   get "arsenal/wilshere", to: "arsenal#wilshere", as:'wilshere'
+   get "arsenal/aubameyang", to: "arsenal#aubameyang", as:'aubameyang'
+   get "arsenal/lacazette", to: "arsenal#lacazette", as:'lacazette'
+   get "arsenal", to: "arsenal#all_arsenal", as: 'all_arsenal'
 
    get "realmadrid/ronaldo", to: "realmadrid#ronaldo" ,as:'ronaldo'
    get "realmadrid/ramos", to: "realmadrid#ramos", as:'ramos'
    get "realmadrid/modric", to: "realmadrid#modric", as:'modric'
    get "realmadrid/kroos", to: "realmadrid#kroos", as:'kroos'
+   get "realmadrid", to: "realmadrid#all_realmadrid", as: 'all_realmadrid'
 
    get "barcelona/messi", to: "barcelona#messi", as:'messi'
    get "barcelona/iniesta", to: "barcelona#iniesta", as:'iniesta'
    get "barcelona/pique", to: "barcelona#pique", as:'pique'
    get "barcelona/suarez", to: "barcelona#suarez", as:'suarez'
+   get "barcelona", to: "barcelona#all_barcelona", as: 'all_barcelona'
 end
